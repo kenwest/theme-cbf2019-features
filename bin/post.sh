67b0b80568a05675df078c15da4146b070f5e0c0
Ken West <ken.west@internode.on.net>
2019-01-29 19:53:35 +1100

    Initial version of the module

---
+++ bin/post.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Run this after recreating the feature.
+# Restore the permissions before committing changes to Git.
+#
+
+chmod g-w -R .
---
+++ bin/pre.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Run this before recreating the feature.
+# Set the permissions so the webserver can update the files.
+#
+
+chmod g+w -R .
